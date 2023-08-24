4b88fa48edc7d19a8beae55adb2b9c64cc3f71b0
wibisana putra setyanegara <eizisana@gmail.com>
2023-08-24 15:40:50 +0700

    add models destinasi

---
+++ src/models/destinasi/destinasiModel.js
@@ -0,0 +1,72 @@
+const mongoose = require("mongoose");
+const mongoosePaginate = require("mongoose-paginate-v2");
+const aggregatePaginate = require("mongoose-aggregate-paginate-v2");
+const uniqueValidator = require("mongoose-unique-validator");
+const mongooseDelete = require("mongoose-delete"); // Added soft delete plugin
+
+const myCustomLabels = {
+  totalDocs: "itemCount",
+  data: "docs",
+  limit: "perPage",
+  page: "currentPage",
+  nextPage: "next",
+  prevPage: "prev",
+  totalPages: "pageCount",
+  pagingCounter: "slNo",
+  meta: "paginator",
+};
+
+mongoosePaginate.paginate.options = { customLabels: myCustomLabels };
+
+const Schema = mongoose.Schema;
+
+const schema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    code: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    address: {
+      type: String,
+    },
+    city: {
+      type: String,
+    },
+    geolocation: {
+      type: Array,
+      required: true,
+      default: [0, 0],
+    },
+    description: {
+      type: String,
+    },
+    visitor: {
+      type: Number,
+      default: 0,
+    },
+  },
+  {
+    timestamps: {
+      createdAt: "createdAt",
+      updatedAt: "updatedAt",
+    },
+  }
+);
+
+schema.index({ "$**": "text" });
+
+schema.method("toJSON", function () {
+  const { _id, __v, ...object } = this.toObject({ virtuals: true });
+  object.id = _id;
+
+  return object;
+});
+schema.plugin(mongoosePaginate);
+schema.plugin(aggregatePaginate);
+const Destinasi = mongoose.model("destinasi", schema);
+module.exports = Destinasi;
